f28edce83840e5953bb73ebf67bcf83eab1a0acc
pushkinss <pushkinss@tripleten.com>
2025-09-10 18:21:07 +0200

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,85 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
+const isCI = process.env.GITHUB_ACTIONS === "true";
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1];
+const base = process.env.VITE_BASE ?? (isCI && repo ? `/${repo}/` : "/");
+
+console.log({ isCI, repo, base });
+
 export default defineConfig({
+  base,
   plugins: [
     react(),
     VitePWA({
+      injectRegister: "auto",
       registerType: "autoUpdate",
+      includeAssets: [
+        "192.png",
+        "256.png",
+        "512.png",
+        "1024.png",
+        "favicon.png",
+        "robots.txt",
+      ],
+      manifest: {
+        name: "Shopper - groccery list",
+        short_name: "Shopper",
+        description: "A simple grocery list app",
+        theme_color: "#C95E34",
+        background_color: "#C95E34",
+        display: "standalone",
+        scope: base,
+        start_url: base,
+        icons: [
+          {
+            src: "192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "1024.png",
+            sizes: "1024x1024",
+            type: "image/png",
+          },
+        ],
+      },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: { cacheName: "html-cache" },
+          },
+          {
+            urlPattern: ({ request }) =>
+              request.destination === "script" ||
+              request.destination === "style",
+            handler: "StaleWhileRevalidate",
+            options: { cacheName: "asset-cache" },
+          },
+          {
+            urlPattern: ({ url }) =>
+              url.origin === self.location.origin &&
+              url.pathname.startsWith("/assets"),
+            handler: "CacheFirst",
+            options: {
+              cacheName: "static-assets",
+              expiration: { maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 30 },
+            },
+          },
+        ],
       },
     }),
   ],
