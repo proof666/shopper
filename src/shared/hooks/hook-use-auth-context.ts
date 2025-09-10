a47780690320defc98bcf3155e210f1d99b2c520
pushkinss <pushkinss@tripleten.com>
2025-09-10 18:29:40 +0200

    deploy

--- src/shared/hooks/hook-use-auth-context.ts
+++ src/shared/hooks/hook-use-auth-context.ts
@@ -1,5 +1,5 @@
 import { createContext, useContext } from "react";
-import type { User } from "../types/index.js";
+import { User } from "../types/user";
 
 type AuthContextType = {
   user: User | null;
